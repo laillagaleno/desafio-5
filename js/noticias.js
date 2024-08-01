32f36a82b2927b51482b5ea1dc9f4409131c2fe2
laillagaleno <laillagalenoitz@gmail.com>
2024-07-31 23:54:56 -0300

    responsividade e page culinaria

--- js/noticias.js
+++ js/noticias.js
@@ -3,55 +3,50 @@ const containerNoticias = document.querySelector(".noticias_container");
 function contruirCard(titulo, descrição, url, imagem, data) {
   const noticia = document.createElement("article");
   noticia.innerHTML = `
-    <a href="${url}">
         <div class="news_data">
-            <div class="news_posted_at">${data}</div>
+         ${data}
         </div>
-        <div class="news_thumb"> <img src="${imagem}" alt=""></div>
+        <div class="news_thumb"><img src="${imagem}" alt=""></div>
         <div class="news_title">
         ${titulo}
         </div>
         <div class="news_resume">
         ${descrição}
         </div>
-    </a>
+        <a class="news_buttom" href="${url}">Ver mais</a>
     `;
 
   return noticia;
 }
 
 async function buscarNoticias() {
-    const conexao = await fetch("http://localhost:3000/noticias");
+  const conexao = await fetch("http://localhost:3000/noticias");
 
-    const conexaoConvertida = await conexao.json();
+  const conexaoConvertida = await conexao.json();
 
-    return conexaoConvertida;
+  return conexaoConvertida;
 }
-
 
 async function noticiasRecentes(quantidade = 4) {
-    try {
-        const noticias = await buscarNoticias();
+  try {
+    const noticias = await buscarNoticias();
 
-        const noticiasFiltradas = noticias.slice(0, quantidade);
+    const noticiasFiltradas = noticias.slice(0, quantidade);
 
-        noticiasFiltradas.forEach((element) =>
-        containerNoticias.appendChild(
-          contruirCard(
-            element.titulo,
-            element.descricao,
-            element.url,
-            element.imagem,
-            element.data
-          )
+    noticiasFiltradas.forEach((element) =>
+      containerNoticias.appendChild(
+        contruirCard(
+          element.titulo,
+          element.descricao,
+          element.url,
+          element.imagem,
+          element.data
         )
-      );
-
-    } catch (error) {
-      containerNoticias.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos</h2>`;
-    }
+      )
+    );
+  } catch (error) {
+    containerNoticias.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos</h2>`;
+  }
 }
-
-
 
 noticiasRecentes(4);
