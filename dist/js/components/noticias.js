22b4db5b29d52e132653edc7bd053d0756c1371c
laillagaleno <laillagalenoitz@gmail.com>
2024-08-02 21:53:26 -0300

    testando rota de noticias

--- dist/js/components/noticias.js
+++ dist/js/components/noticias.js
@@ -1,7 +1,8 @@
-const containerNoticias = document.querySelector(".noticias_container");
-function contruirCardNoticia(titulo, descrição, url, imagem, data) {
-    const noticia = document.createElement("article");
-    noticia.innerHTML = `
+window.onload = function () {
+    const containerNoticias = document.querySelector(".noticias_container");
+    function contruirCardNoticia(titulo, descrição, url, imagem, data) {
+        const noticia = document.createElement("article");
+        noticia.innerHTML = `
         <div class="news_data">
          ${data}
         </div>
@@ -14,21 +15,22 @@ function contruirCardNoticia(titulo, descrição, url, imagem, data) {
         </div>
         <a class="news_buttom" href="${url}">Ver mais</a>
     `;
-    return noticia;
-}
-async function buscarNoticias() {
-    const conexao = await fetch("http://localhost:3000/noticias");
-    const conexaoConvertida = await conexao.json();
-    return conexaoConvertida;
-}
-async function noticiasRecentes(quantidade = 4) {
-    try {
-        const noticias = await buscarNoticias();
-        const noticiasFiltradas = noticias.slice(0, quantidade);
-        noticiasFiltradas.forEach((element) => containerNoticias.appendChild(contruirCardNoticia(element.titulo, element.descricao, element.url, element.imagem, element.data)));
+        return noticia;
     }
-    catch (error) {
-        containerNoticias.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos</h2>`;
+    async function buscarNoticias() {
+        const conexao = await fetch("http://localhost:3000/noticias");
+        const conexaoConvertida = await conexao.json();
+        return conexaoConvertida;
     }
-}
-noticiasRecentes(4);
+    async function noticiasRecentes(quantidade = 4) {
+        try {
+            const noticias = await buscarNoticias();
+            const noticiasFiltradas = noticias.slice(0, quantidade);
+            noticiasFiltradas.forEach((element) => containerNoticias.appendChild(contruirCardNoticia(element.titulo, element.descricao, element.url, element.imagem, element.data)));
+        }
+        catch (error) {
+            containerNoticias.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos</h2>`;
+        }
+    }
+    noticiasRecentes(4);
+};
--- src/components/noticias.ts
+++ src/components/noticias.ts
@@ -1,8 +1,15 @@
-const containerNoticias = document.querySelector(".noticias_container");
+window.onload = function () {
+  const containerNoticias = document.querySelector(".noticias_container");
 
-function contruirCardNoticia(titulo: string, descrição: string, url:string, imagem:string, data: string) {
-  const noticia = document.createElement("article");
-  noticia.innerHTML = `
+  function contruirCardNoticia(
+    titulo: string,
+    descrição: string,
+    url: string,
+    imagem: string,
+    data: string
+  ) {
+    const noticia = document.createElement("article");
+    noticia.innerHTML = `
         <div class="news_data">
          ${data}
         </div>
@@ -16,43 +23,45 @@ function contruirCardNoticia(titulo: string, descrição: string, url:string, im
         <a class="news_buttom" href="${url}">Ver mais</a>
     `;
 
-  return noticia;
-}
-
-async function buscarNoticias() {
-  const conexao = await fetch("http://localhost:3000/noticias");
-
-  const conexaoConvertida = await conexao.json();
-
-  return conexaoConvertida;
-}
-
-async function noticiasRecentes(quantidade = 4) {
-  try {
-    const noticias = await buscarNoticias();
-
-    const noticiasFiltradas = noticias.slice(0, quantidade);
-
-    noticiasFiltradas.forEach((element:{
-      titulo: string,
-      descricao: string,
-      url: string,
-      imagem: string,
-      data: string
-    }) =>
-      containerNoticias.appendChild(
-        contruirCardNoticia(
-          element.titulo,
-          element.descricao,
-          element.url,
-          element.imagem,
-          element.data
-        )
-      )
-    );
-  } catch (error) {
-    containerNoticias.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos</h2>`;
+    return noticia;
   }
-}
 
-noticiasRecentes(4);
+  async function buscarNoticias() {
+    const conexao = await fetch("http://localhost:3000/noticias");
+
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+  }
+
+  async function noticiasRecentes(quantidade = 4) {
+    try {
+      const noticias = await buscarNoticias();
+
+      const noticiasFiltradas = noticias.slice(0, quantidade);
+
+      noticiasFiltradas.forEach(
+        (element: {
+          titulo: string;
+          descricao: string;
+          url: string;
+          imagem: string;
+          data: string;
+        }) =>
+          containerNoticias.appendChild(
+            contruirCardNoticia(
+              element.titulo,
+              element.descricao,
+              element.url,
+              element.imagem,
+              element.data
+            )
+          )
+      );
+    } catch (error) {
+      containerNoticias.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos</h2>`;
+    }
+  }
+
+  noticiasRecentes(4);
+};
